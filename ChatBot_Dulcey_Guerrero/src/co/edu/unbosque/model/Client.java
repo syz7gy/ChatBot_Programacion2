59aec9dcec8febb9b047740eea1f28cd8c2b482a
P <paula.guerrero.pineda@gmail.com>
2023-08-22 23:29:30 -0500

    Changed Client and Server class
    Added few things in Client and Server class, also in the txt.

--- ChatBot_Dulcey_Guerrero/src/co/edu/unbosque/model/Client.java
+++ ChatBot_Dulcey_Guerrero/src/co/edu/unbosque/model/Client.java
@@ -1,28 +1,24 @@
 package co.edu.unbosque.model;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client extends Thread {
 
 	protected Socket s;
-	protected ServerSocket ss;
 	private Scanner sc;
-	protected ObjectOutputStream out;
-	protected ObjectInputStream in;
+	protected DataOutputStream out;
+	protected DataInputStream in;
 	private String address;
 	private int port;
 
 	public Client(String address, int port) {
-		this.s = null;
-		this.ss = null;
 		this.sc = new Scanner(System.in);
-		this.out = null;
 		this.address = address;
 		this.port = port;
 	}
@@ -33,32 +29,18 @@ public class Client extends Thread {
 		String line;
 
 		try {
-			this.s = new Socket(this.address, this.port);
+			s = new Socket(this.address, this.port);
 			System.out.println("Connected");
 
-			this.out = new ObjectOutputStream(s.getOutputStream());
-
-			line = sc.nextLine();
-			this.out.writeUTF(line);
-
-			this.out.close();
-			this.s.close();
-			this.ss = new ServerSocket(this.port + 1);
-			System.out.println("Recieved message: ");
-			this.in = new ObjectInputStream(new BufferedInputStream(s.getInputStream()));
+			out = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+			in = new DataInputStream(new BufferedInputStream(s.getInputStream()));
 			System.out.println(in.readUTF());
-			this.in.close();
-			this.ss.close();
-			
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+			line = sc.nextLine();
 
-		try {
-			this.out.close();
-			s.close();
+			out.writeUTF(line);
+
 		} catch (IOException e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
 	}
 }
--- ChatBot_Dulcey_Guerrero/src/co/edu/unbosque/model/Server.java
+++ ChatBot_Dulcey_Guerrero/src/co/edu/unbosque/model/Server.java
@@ -1,58 +1,123 @@
 package co.edu.unbosque.model;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//import co.edu.unbosque.model.persistance.FileHandler;
+
 public class Server extends Thread {
-	
+
 	protected Socket s;
 	protected Socket replyS;
 	protected ServerSocket ss;
-	protected ObjectOutputStream out;
-	protected ObjectInputStream in;
+	protected DataOutputStream out;
+	protected DataInputStream in;
 	private int port;
 	private String clientAddress;
-	private static File file;
 	private static Scanner sc;
+	private static File file;
 	private ArrayList<String> answers;
-	
+
 	public Server(int port) {
-		this.s = null;
-		this.replyS = null;
-		this.ss = null;
-		this.out = null;
-		this.in = null;
 		this.port = port;
 		this.clientAddress = clientAddress;
-		
 		file = new File("src/co/edu/unbosque/model/persistance/Answers.txt");
 		answers = new ArrayList<>();
 		try {
 			sc = new Scanner(file);
 		} catch (FileNotFoundException e) {
-			System.out.println("Error: The file 'Answers.txt' was not found.");
+			System.out.println("Error: File 'Answers.txt' was not found.");
 			e.printStackTrace();
-		} catch(IOException e) {
-			System.out.println(
-					"Error: The file 'Answers.txt' couldn't be opened. Check permissions.");
+		} catch (IOException e) {
+			System.out.println("Error: Check permissions for file 'Answers.txt'.");
 			e.printStackTrace();
 		}
-		while(sc.hasNext()) {
+		while (sc.hasNext()) {
 			answers.add(sc.nextLine());
-		}	
+		}
+
 	}
-	
+
 	@Override
 	public void run() {
-		
-		System.out.println(answers.toString());
-		
+
+		try {
+			ss = new ServerSocket(port);
+			System.out.println("Server started");
+			System.out.println("Waiting for a client to connect...");
+			s = ss.accept();
+			out = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
+			in = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+			System.out.println(
+					"Client accepted. " + "\nWelcome to Paulita's and Juanchito's chatbot." + "\nSelenct an option:");
+			System.out.println(menu().toString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public StringBuilder menu() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n1) Who's your favorite artist?");
+		sb.append("\n2) What's the best alt rock band?");
+		sb.append("\n3) Who's the musician you have on repeat?");
+		sb.append("\n4) Modern Warfare 2019 or Modern Warfare 2?");
+		sb.append("\n5) Taylor Swift or Kanye West?");
+		sb.append("\n6) Have you ever commited murder?");
+		sb.append("\n7) Favourite sex position?");
+		sb.append("\n8) Who made a revolution in musical industry?");
+		sb.append("\n9) What's the best historical period?");
+		sb.append("\n10) Yo Mr. White?");
+		sb.append("\n11) ?");
+		sb.append("\n12) Who was Stalin?");
+		sb.append("\n13) Are you gay?");
+		sb.append("\n14) Will someone love me?");
+		sb.append("\n15) Whats the best Sci-Fi series?");
+		sb.append("\n16) Who's the best teacher?");
+		sb.append("\n17) Who's the worst teacher?");
+		sb.append("\n18) Do you wanna build a snowman?");
+		sb.append("\n19) What if the earth ends tomorrow?");
+		sb.append("\n20) I might be wrong?");
+		sb.append("\n21) Close chat.");
+		return sb;
+	}
+
+	private String botAnswers(String rta) {
+		int resp = Integer.parseInt(rta);
+		switch (resp) {
+		case 1:
+			return null;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+
+		}
+		return rta;
 	}
 }
